0fb761298190430f614679b1b6c76aa209128b89
Jesus <jesus.martinez.eece@gmail.com>
2026-09-06 18:46:22 -0500

    corrected logic for toptrend and completed top tree_trends

--- HW_1/trendtracker.cpp
+++ HW_1/trendtracker.cpp
@@ -11,12 +11,7 @@ Trendtracker :: Trendtracker()//O(1) constant
 }
 
 void Trendtracker :: insert(string ht)
-{
-    //maybe ill make it a smart insert nvm doesnt make sense as
-    // the popularity changes the list cant be ordered until i have all entries 
-    //add a check to see if it already is in trendtracker
-    //can do a for loop which is O(n) maybe 
-        //in the loop have a add if it wasnt found O(1) 
+{   //Goal: add new entry if not in E 
     for (int i = 0; i < E.size() ; i++ ) //O()
     {
         if ( E[i].hashtag == ht)
@@ -30,38 +25,90 @@ void Trendtracker :: insert(string ht)
 
 int Trendtracker :: size() // O(1) 
 {
-    //check the size of the vector
+    //Goal: check the size of the vector
     return E.size(); // .size() is O(1) check as to why that is
 }
 
 void Trendtracker:: tweeted(string ht)
 {
-    //add 1 to pop 
+    //Goal: Add 1 to pop
     //find the hastag 
     for (int i = 0; i < E.size(); i ++ )
     {
         if ( ht == E[i].hashtag) 
-            E[i].pop ++; //i recall in CSI we spoke of ++x or x++
+            E[i].pop ++; 
+            //i recall in CSI we spoke of ++x or x++
     }
-
     return ; //if hashtag does not exist
-
 }
 
 int Trendtracker:: popularity(string name)
 {
-    //this is E.pop check 
-    //loop through vector check the name == E[i].hashtag if yes then check the
-    //E.[i].pop
+    //Goal: return E.pop
+    for (int i = 0; i < E.size(); i++)
+    {
+        if (E[i].hashtag == name)
+        {
+            return E[i].pop;
+        }
+    }
+    return -1; //if for loop runs and no match is found 
 }
 
 string Trendtracker:: top_trend()
 {
+    //check if trendtracker is empty
+    if (E.empty())
+        return "";
+    else //check for largest pop
+    {
+        for (int i =0; i < E.size(); i++)
+        {
+            int top_pop = 0;  
+
+            if (E[top_pop].pop < E[i].pop ) //what do i do in equal condition ? do i update or not to make <=
+            {
+                top_pop = i;
+            }             
+            return E[top_pop].hashtag; 
+
+        }
+    }
 
 }
 
 void Trendtracker:: top_three_trends(vector<string> &T)
 {
+    //goal find the top three trends
+    if (E.empty()) //i infered this from the previous case 
+        T = {"", "", ""};
+
+    else //check for 3 largest pops
+    {
+        for (int i =0; i < E.size(); i++)
+        {
+            int first_pop = 0; 
+            int second_pop  = 0; 
+            int third_pop = 0;
+
+            if (E[first_pop].pop < E[i].pop)
+            {
+                first_pop = i;
+            }             
+            else if (E[second_pop].pop < E[i].pop)
+            {
+                second_pop = i;
+            }
+            else if(E[third_pop].pop < E[i].pop)
+            {
+                third_pop = i; 
+            }
+            
+            T = {E[first_pop].hashtag, E[second_pop].hashtag, E[third_pop].hashtag}; 
+
+        }
+    }
+
 
 }
 
