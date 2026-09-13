21dac6e9e98621f65990adadd8ea95b07e02ec5d
Jesus <jesus.martinez.eece@gmail.com>
2026-09-13 14:10:55 -0500

    HW2 CSCI 3333

---
+++ HW_2/trendtracker.cpp
@@ -0,0 +1,44 @@
+#include "trendtracker.h"
+
+Trendtracker :: Trendtracker(string filename)
+{
+/*so needs to create trendtracker and fill it with the 
+hashtags in the provided file 
+must run in O(n) - assumin this means pass by ref as a 
+pass by value as we discussed is O(n) so for O(1) pass by 
+ref and make everything in the method constant runtime 
+*/
+}
+
+int Trendtracker :: size() 
+{
+    E.size(); //O(1)
+
+}
+
+void Trendtracker :: tweeted(string ht)
+{
+
+}
+
+int Trendtracker :: popularity(string name) 
+{
+
+}
+
+string Trendtracker :: top_trend()
+{
+
+
+}
+
+void Trendtracker :: top_three_trends(vector<string> &T) 
+{
+
+
+}
+
+int Trendtracker :: search(string ht)
+{
+
+}
